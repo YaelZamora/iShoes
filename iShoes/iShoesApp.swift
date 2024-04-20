22227801a5c0668cc974a67cf0c8bcf1da2abd59
Yael Zamora <ajedrezkasparov@gmail.com>
2024-04-20 17:07:00 -0600

    Initial Commit

---
+++ iShoes/iShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShoesApp.swift
+//  iShoes
+//
+//  Created by Yael Javier Zamora Moreno on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
